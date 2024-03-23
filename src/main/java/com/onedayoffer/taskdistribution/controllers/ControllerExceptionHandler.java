a225ef02b9ea3c30be694207aaa4046af595172a
Maria Azarenok <maria.azarenok2691@gmail.com>
2024-03-23 11:06:17 +0300

    Реализованы функциональные требования + добавлена обработка исключений

--- src/main/java/com/onedayoffer/taskdistribution/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/onedayoffer/taskdistribution/controllers/ControllerExceptionHandler.java
@@ -1,11 +1,34 @@
 package com.onedayoffer.taskdistribution.controllers;
 
-import org.springframework.http.HttpHeaders;
+import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//@ControllerAdvice(annotations= RestController.class)
+
+
+@RestControllerAdvice
 public class ControllerExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException ex) {
+        logger.error("Entity not found: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        logger.error("Illegal argument: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralError(Exception ex) {
+        logger.error("An error occurred: {}", ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An internal server error occurred.");
+    }
 }
